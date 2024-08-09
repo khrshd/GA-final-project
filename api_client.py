17ff060236027506483c5d1c87ea6dc7ee5508f8
khrshd <khurshid.x@gmail.com>
2024-08-09 00:12:09 -0400

    updated api key

--- api_client.py
+++ api_client.py
@@ -4,11 +4,11 @@ import json
 class weather_api_client:
     def __init__(self):
         self.api_url_base = 'api.openweathermap.org/data/2.5/'
-        self.api_key = '623af9cc19f6aab782588ed432ed3434'
+        self.api_key = 'your-api-key'
     
     def get_weather(self, city_name):
         
-        # https://api.openweathermap.org/data/2.5/weather?q=Boston,US&appid=623af9cc19f6aab782588ed432ed3434
+        # https://api.openweathermap.org/data/2.5/weather?q=Boston,US&appid=your-api-key
 
         api_url = '{0}weather?q={1}&appId={2}'.format(self.api_url_base, city_name, self.api_key)
                 
@@ -27,7 +27,7 @@ class weather_api_client:
 
     def get_forecast(self, city_name):
         
-        # https://api.openweathermap.org/data/2.5/forecast?q=Boston,US&appid=623af9cc19f6aab782588ed432ed3434
+        # https://api.openweathermap.org/data/2.5/forecast?q=Boston,US&appid=your-api-key
 
         api_url = '{0}forecast?q={1}&appId={2}'.format(self.api_url_base, city_name, self.api_key)
                 
